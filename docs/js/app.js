5c7f84d4c812d88b09d8e5ea8345ced56eec2c5c
vincentsebag <vincentsebag+git@gmail.com>
2024-02-11 20:40:49 +0100

    Change of colors

--- docs/js/app.js
+++ docs/js/app.js
@@ -180,7 +180,7 @@ const progress = (() => {
         loaded += 1;
 
         bar.style.width = Math.min((loaded / total) * 100, 100).toString() + "%";
-        info.innerText = `Le pigeon voyageur arrive dans (${loaded}/${total}) [${parseInt(bar.style.width).toFixed(0)}%]`;
+        info.innerText = `Le pigeon voyageur arrive dans ${loaded}/${total} [${parseInt(bar.style.width).toFixed(0)}%]`;
 
         if (loaded == total) {
             util.show();
