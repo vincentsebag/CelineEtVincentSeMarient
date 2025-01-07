0f547abd7b1b9d4a52596700d27a13117d226ab2
vincentsebag <vincentsebag+git@gmail.com>
2025-01-07 15:48:00 +0100

    restructuration

--- docs/js/script.js
+++ docs/js/script.js
@@ -5,7 +5,7 @@
   app.config(function ($routeProvider) {
     $routeProvider
       .when("/", {
-        templateUrl: "home.html",
+        templateUrl: "restructuration.html", //home.html
       })
       .when("/programme", {
         templateUrl: "programme.html",
